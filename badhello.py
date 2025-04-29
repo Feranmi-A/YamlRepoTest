8ee379ee9fe4e1abe724d6dadf2f53f54be72fcc
Feranmi <Oige5@rrc.ca>
2025-04-28 21:47:51 -0500

    new added files

---
+++ badhello.py
@@ -0,0 +1,10 @@
+def hello():
+    print("hi") # There should be two empty lines after each function 
+ 
+def bye():
+  print("bye") # the indentation here is just 2 spaces  
+
+
+
+
+print(hello())  # there are more than 2 empty lines before calling the function
